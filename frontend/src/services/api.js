c69c5860b1dcb2ebf285d15bc4debfbd5b68c12b
PriyaSingh52 <priyasingh20102020@gmail.com>
2026-04-10 16:41:32 +0530

    first commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://clinic-website-cms.onrender.com/api"
+  baseURL: "https://clinic-website-cms-ovo4.onrender.com/api"
 });
 
 export default API;
